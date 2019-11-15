6eca11f3b1dd22f63eef2f9b770dd5935dc771e9
Rodrigo Naya <rodrigoezequielnaya@gmail.com>
2019-11-15 18:27:08 -0300

    Empty ChatBotActivity

--- app/src/main/java/ai/akun/akunsports/MainActivity.kt
+++ app/src/main/java/ai/akun/akunsports/MainActivity.kt
@@ -1,12 +1,20 @@
 package ai.akun.akunsports
 
+import ai.akun.nukasdk.ChatBotActivity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openChatBot.setOnClickListener {
+            startActivity(Intent(this, ChatBotActivity::class.java))
+        }
     }
+
 }
---
+++ nukasdk/src/main/java/ai/akun/nukasdk/ChatBotActivity.kt
@@ -0,0 +1,12 @@
+package ai.akun.nukasdk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatBotActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_bot)
+    }
+}
