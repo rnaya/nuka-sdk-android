a3b6b7dce77642929bf3d398fa8389bb2b80b3e3
Rodrigo Naya <rodrigoezequielnaya@gmail.com>
2019-11-16 12:10:02 -0300

    Added toolbar

--- nukasdk/src/main/java/ai/akun/nukasdk/ChatBotActivity.kt
+++ nukasdk/src/main/java/ai/akun/nukasdk/ChatBotActivity.kt
@@ -2,11 +2,22 @@ package ai.akun.nukasdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_chat_bot.*
 
 class ChatBotActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_bot)
+        setUpToolbar()
+    }
+
+    private fun setUpToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
