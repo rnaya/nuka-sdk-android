a81c6e76a824d29128968533072874df86710dfc
Rodrigo Naya <rodrigoezequielnaya@gmail.com>
2019-11-16 17:17:58 -0300

    Added button to open chat in app activity

--- app/src/main/java/ai/akun/akunsports/MainActivity.kt
+++ app/src/main/java/ai/akun/akunsports/MainActivity.kt
@@ -1,9 +1,11 @@
 package ai.akun.akunsports
 
+
 import ai.akun.nukasdk.chatbot.presentation.ChatBotActivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         startActivity(Intent(this, ChatBotActivity::class.java))
+
+        openChatBot.setOnClickListener {
+            startActivity(Intent(this, ChatBotActivity::class.java))
+        }
     }
 
 }
