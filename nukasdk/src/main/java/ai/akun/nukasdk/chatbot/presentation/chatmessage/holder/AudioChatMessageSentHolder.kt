c247fbb0882cc836cce142099a5da35a9bc686c3
Rodrigo Naya <rodrigoezequielnaya@gmail.com>
2019-11-23 15:15:21 -0300

    Audio chat message WIP

--- nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/chatmessage/holder/AudioChatMessageSentHolder.kt
+++ nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/chatmessage/holder/AudioChatMessageSentHolder.kt
@@ -4,17 +4,29 @@ import ai.akun.nukasdk.chatbot.presentation.main.ChatMessage
 import android.media.MediaPlayer
 import android.view.View
 import kotlinx.android.synthetic.main.item_row_audio_chat_message_sent.view.*
-import kotlinx.android.synthetic.main.item_row_text_chat_message_sent.view.*
+import java.util.concurrent.TimeUnit
 
 class AudioChatMessageSentHolder(itemView: View) : ChatMessageHolder(itemView) {
 
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun bind(chatMessage: ChatMessage) {
+        mediaPlayer = MediaPlayer()
+        mediaPlayer.setDataSource(chatMessage.audioFilePath)
+        mediaPlayer.prepare()
+
+        setAudioDuration()
+
         itemView.play.setOnClickListener {
-            val mediaPlayer = MediaPlayer()
-            mediaPlayer.setDataSource(chatMessage.audioFilePath)
-            mediaPlayer.prepare()
             mediaPlayer.start()
         }
     }
 
+    private fun setAudioDuration() {
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(mediaPlayer.duration.toLong())
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(
+            mediaPlayer.duration.toLong() - TimeUnit.MINUTES.toMillis(minutes)
+        )
+        itemView.audioDuration.text = String.format("%02d:%02d",minutes, seconds)
+    }
 }
