07a10ca7fbad5affaa9a43d884f9cc7b393a1c08
Rodrigo Naya <rodrigoezequielnaya@gmail.com>
2019-11-24 14:58:27 -0300

    Audio chat message seekbar progress

--- nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/chatmessage/holder/AudioChatMessageSentHolder.kt
+++ nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/chatmessage/holder/AudioChatMessageSentHolder.kt
@@ -2,13 +2,23 @@ package ai.akun.nukasdk.chatbot.presentation.chatmessage.holder
 
 import ai.akun.nukasdk.chatbot.presentation.main.ChatMessage
 import android.media.MediaPlayer
+import android.os.Handler
 import android.view.View
 import kotlinx.android.synthetic.main.item_row_audio_chat_message_sent.view.*
 import java.util.concurrent.TimeUnit
 
+
 class AudioChatMessageSentHolder(itemView: View) : ChatMessageHolder(itemView) {
 
     private lateinit var mediaPlayer: MediaPlayer
+
+    private val audioSeekBarHandler = Handler()
+    private val updateAudioSeekBarRunnable: Runnable = object : Runnable {
+        override fun run() {
+            itemView.audioProgressSeekBar.progress = mediaPlayer.currentPosition
+            audioSeekBarHandler.postDelayed(this, 15)
+        }
+    }
 
     override fun bind(chatMessage: ChatMessage) {
         mediaPlayer = MediaPlayer()
@@ -18,21 +28,39 @@ class AudioChatMessageSentHolder(itemView: View) : ChatMessageHolder(itemView) {
         setAudioDuration()
 
         itemView.play.setOnClickListener {
-            mediaPlayer.start()
-            it.visibility = View.GONE
-            itemView.pause.visibility = View.VISIBLE
+            playAudio()
         }
 
         itemView.pause.setOnClickListener {
-            mediaPlayer.pause()
-            it.visibility = View.GONE
-            itemView.play.visibility = View.VISIBLE
+            pauseAudio()
         }
 
         mediaPlayer.setOnCompletionListener {
-            itemView.play.visibility = View.VISIBLE
-            itemView.pause.visibility = View.GONE
+            resetAudio()
         }
+
+        itemView.audioProgressSeekBar.setOnTouchListener { _, _ -> true }
+    }
+
+    private fun playAudio() {
+        mediaPlayer.start()
+        itemView.play.visibility = View.GONE
+        itemView.pause.visibility = View.VISIBLE
+        audioSeekBarHandler.postDelayed(updateAudioSeekBarRunnable, 15)
+    }
+
+    private fun pauseAudio() {
+        mediaPlayer.pause()
+        itemView.pause.visibility = View.GONE
+        itemView.play.visibility = View.VISIBLE
+        audioSeekBarHandler.removeCallbacks(updateAudioSeekBarRunnable)
+    }
+
+    private fun resetAudio() {
+        itemView.play.visibility = View.VISIBLE
+        itemView.pause.visibility = View.GONE
+        audioSeekBarHandler.removeCallbacks(updateAudioSeekBarRunnable)
+        itemView.audioProgressSeekBar.progress = 0
     }
 
     private fun setAudioDuration() {
@@ -41,5 +69,7 @@ class AudioChatMessageSentHolder(itemView: View) : ChatMessageHolder(itemView) {
             mediaPlayer.duration.toLong() - TimeUnit.MINUTES.toMillis(minutes)
         )
         itemView.audioDuration.text = String.format("%02d:%02d",minutes, seconds)
+        itemView.audioProgressSeekBar.progress = 0
+        itemView.audioProgressSeekBar.max = mediaPlayer.duration
     }
 }
