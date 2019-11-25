f357e5208ad73b0744eaa6f6fc4dcac909712673
Rodrigo Naya <rodrigoezequielnaya@gmail.com>
2019-11-25 11:34:39 -0300

    ImageLoader for gifs

--- nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/chatmessage/holder/ReceivedGifChatMessageViewHolder.kt
+++ nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/chatmessage/holder/ReceivedGifChatMessageViewHolder.kt
@@ -2,6 +2,7 @@ package ai.akun.nukasdk.chatbot.presentation.chatmessage.holder
 
 import ai.akun.nukasdk.chatbot.presentation.main.ChatBotViewModel
 import ai.akun.nukasdk.chatbot.presentation.main.ChatMessage
+import ai.akun.nukasdk.chatbot.presentation.shared.ImageLoader
 import android.view.View
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_row_received_gif_chat_message.view.*
@@ -9,10 +10,7 @@ import kotlinx.android.synthetic.main.item_row_received_gif_chat_message.view.*
 class ReceivedGifChatMessageViewHolder(itemView: View) : ChatMessageViewHolder(itemView) {
 
     override fun bind(chatMessage: ChatMessage) {
-        //TODO loading and error handling. Play/pause??
-        Glide.with(itemView.context)
-            .load(chatMessage.imageUri)
-            .into(itemView.gif)
+        ImageLoader.load(chatMessage.imageUri!!, itemView.gif)
     }
 
     override fun setViewModel(chatBotViewModel: ChatBotViewModel) { }
---
+++ nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/shared/ImageLoader.kt
@@ -0,0 +1,27 @@
+package ai.akun.nukasdk.chatbot.presentation.shared
+
+import ai.akun.nukasdk.R
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+
+class ImageLoader {
+
+    companion object {
+
+        fun load(imageUrl: String, imageView: ImageView) {
+            val circularProgressDrawable = CircularProgressDrawable(imageView.context)
+            circularProgressDrawable.strokeWidth = 10f
+            circularProgressDrawable.centerRadius = 25f
+            circularProgressDrawable.setColorSchemeColors(R.color.accentBlue)
+            circularProgressDrawable.start()
+
+            Glide.with(imageView.context)
+                .load(imageUrl)
+                .placeholder(circularProgressDrawable)
+                .into(imageView)
+        }
+
+    }
+
+}
