4aff321df1fdce4002c28a60f2924ecbb43bc68c
Rodrigo Naya <rodrigoezequielnaya@gmail.com>
2019-11-29 18:06:17 -0300

    Welcome message action updated

--- nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/chatmessage/holder/ReceivedWelcomeTextChatMessageViewHolder.kt
+++ nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/chatmessage/holder/ReceivedWelcomeTextChatMessageViewHolder.kt
@@ -19,17 +19,17 @@ class ReceivedWelcomeTextChatMessageViewHolder(itemView: View) : ChatMessageView
         itemView.content.text = chatMessage.text
 
         itemView.matchesAction.setOnClickListener {
-            val action = itemView.context.getString(R.string.next_match)
+            val action = itemView.context.getString(R.string.next_match_question)
             addNewActionChatMessage(action)
         }
 
         itemView.positionsAction.setOnClickListener {
-            val action = itemView.context.getString(R.string.positions)
+            val action = itemView.context.getString(R.string.table_question)
             addNewActionChatMessage(action)
         }
 
         itemView.playersAction.setOnClickListener {
-            val action = itemView.context.getString(R.string.players)
+            val action = itemView.context.getString(R.string.players_question)
             addNewActionChatMessage(action)
         }
     }
