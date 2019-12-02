5710ff849f6f1e057da6b8667dbd94f7f0c233d2
Rodrigo Naya <rodrigoezequielnaya@gmail.com>
2019-12-02 20:18:22 -0300

    Mock live updates

--- nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/chatmessage/holder/ReceivedLiveMatchAvailableTextChatMessageViewHolder.kt
+++ nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/chatmessage/holder/ReceivedLiveMatchAvailableTextChatMessageViewHolder.kt
@@ -2,6 +2,7 @@ package ai.akun.nukasdk.chatbot.presentation.chatmessage.holder
 
 import ai.akun.nukasdk.chatbot.presentation.main.ChatMessage
 import ai.akun.nukasdk.chatbot.presentation.main.ChatMessageIntent
+import android.os.Handler
 import android.view.View
 import kotlinx.android.synthetic.main.item_row_received_live_match_available_text_chat_message.view.*
 import kotlinx.android.synthetic.main.item_row_sent_text_chat_message.view.content
@@ -15,16 +16,28 @@ class ReceivedLiveMatchAvailableTextChatMessageViewHolder(itemView: View) : Chat
         itemView.content.text = chatMessage.text
 
         itemView.yesAction.setOnClickListener {
-            addNewActionChatMessage("Ok")
+            addNewActionChatMessage("Ok", ChatMessageIntent.RECEIVED_TEXT)
+
+            Handler().postDelayed({
+                addNewActionChatMessage("Sebastian Mendez", ChatMessageIntent.RECEIVED_TEXT)
+            }, 2000)
+
+            Handler().postDelayed({
+                addNewActionChatMessage("Jaime Valdes", ChatMessageIntent.RECEIVED_TEXT)
+            }, 4000)
+
+            Handler().postDelayed({
+                addNewActionChatMessage("Entra: Sergio Bareiro \n Sale: Javier Urzua", ChatMessageIntent.RECEIVED_TEXT)
+            }, 6000)
         }
 
         itemView.noAction.setOnClickListener {
-            addNewActionChatMessage("Ok")
+            addNewActionChatMessage("Ok", ChatMessageIntent.RECEIVED_TEXT)
         }
     }
 
-    private fun addNewActionChatMessage(actionText: String) {
-        onSendNewMessage.invoke(actionText, ChatMessageIntent.RECEIVED_TEXT)
+    private fun addNewActionChatMessage(actionText: String, chatMessageIntent: ChatMessageIntent) {
+        onSendNewMessage.invoke(actionText, chatMessageIntent)
     }
 
 }
