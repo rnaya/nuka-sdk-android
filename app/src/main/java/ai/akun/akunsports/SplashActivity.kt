3fca722e4a68359ba4ce03ffdb12e1e850582842
Rodrigo Naya <rodrigoezequielnaya@gmail.com>
2019-12-05 17:15:46 -0300

    Splash activity

---
+++ app/src/main/java/ai/akun/akunsports/SplashActivity.kt
@@ -0,0 +1,26 @@
+package ai.akun.akunsports
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    private val splashTime = 3000L //3 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            navigateToMainActivity()
+        }, splashTime)
+    }
+
+    private fun navigateToMainActivity() {
+        val mainActivityIntent = Intent(this, MainActivity::class.java)
+        startActivity(mainActivityIntent)
+        finish()
+    }
+}
