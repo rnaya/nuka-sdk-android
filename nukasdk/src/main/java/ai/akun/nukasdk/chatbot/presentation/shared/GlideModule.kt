566e34fca584c995272b96ff849b78e277215a8f
Rodrigo Naya <rodrigoezequielnaya@gmail.com>
2019-12-10 15:21:44 -0300

    Disabled SSL checks in Glide

--- nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/shared/GlideModule.kt
+++ nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/shared/GlideModule.kt
@@ -1,5 +1,6 @@
 package ai.akun.nukasdk.chatbot.presentation.shared
 
+import android.annotation.SuppressLint
 import android.content.Context
 import com.bumptech.glide.Glide
 import com.bumptech.glide.Registry
@@ -9,13 +10,47 @@ import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
 import okhttp3.OkHttpClient
 import java.io.InputStream
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLContext
+import javax.net.ssl.X509TrustManager
 
 @GlideModule
-class UnsafeGlideModule : AppGlideModule() {
+class SSLDisabledGlideModule : AppGlideModule() {
 
     override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
-        val okHttpClient: OkHttpClient = RetrofitBuilder.getInstance(context).build()
+        val okHttpClient: OkHttpClient = getUnsafeOkHttpClient()
         registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(okHttpClient))
+    }
+
+    private fun getUnsafeOkHttpClient(): OkHttpClient {
+        val okHttpClientBuilder = OkHttpClient.Builder()
+        trustAllSslCertificates(okHttpClientBuilder)
+        return okHttpClientBuilder.build()
+    }
+
+    private fun trustAllSslCertificates(okHttpClientBuilder: OkHttpClient.Builder) {
+        val trustAllCerts = arrayOf(object : X509TrustManager {
+            @SuppressLint("TrustAllX509TrustManager")
+            override fun checkClientTrusted(p0: Array<out X509Certificate>?, p1: String?) {
+            }
+
+            @SuppressLint("TrustAllX509TrustManager")
+            override fun checkServerTrusted(p0: Array<out X509Certificate>?, p1: String?) {
+            }
+
+            override fun getAcceptedIssuers(): Array<X509Certificate> {
+                return arrayOf()
+            }
+        })
+
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+
+        val sslSocketFactory = sslContext.socketFactory
+        okHttpClientBuilder.sslSocketFactory(sslSocketFactory, trustAllCerts[0])
+        okHttpClientBuilder.hostnameVerifier(HostnameVerifier { _, _ -> true })
     }
 
 }
