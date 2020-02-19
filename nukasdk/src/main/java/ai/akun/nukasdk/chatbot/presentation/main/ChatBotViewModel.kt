cd2734e1397a0b1af5c6d4643b9060b24c249232
Rodrigo Naya <rodrigoezequielnaya@gmail.com>
2020-02-18 21:59:04 -0300

    Fix language code to "es"

--- nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/main/ChatBotViewModel.kt
+++ nukasdk/src/main/java/ai/akun/nukasdk/chatbot/presentation/main/ChatBotViewModel.kt
@@ -128,7 +128,8 @@ class ChatBotViewModel @Inject constructor(private val context: Context,
     }
 
     private fun getCurrentLocale(): String {
-        return if (BuildConfig.DEBUG) "es" else ConfigurationCompat.getLocales(context.resources.configuration)[0].language
+        //return if (BuildConfig.DEBUG) "es" else ConfigurationCompat.getLocales(context.resources.configuration)[0].language
+        return "es"
     }
 
     fun sendLiveMatchUpdatesRequest(matchId: String) {
